cc46375b5d869eaf887e6a48040d22cb21b8cefa
Iggu18 <ignatiousbenoyputhettu100@gmail.com>
2025-11-20 17:02:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
-function setup() {
-  // create Black Cat
-  let blackCat = new Cat("Blacky", "straight");
-  let redCat = new Cat("Tiger");
-  
-  //let them talk
-  blackCat.talk();
-  blackCat.expressYourself();
-  
-  redCat.talk();
+// SeaLion.js
+
+class SeaLion extends Animal {
+  constructor (name,species,sound){
+    this.name="Mr.waggy bloog"
+    this.species = "Sea Lion";
+    this.sound = "Ow ow ow (sea dog energy)";
+    this.canFly = false;
+    this.canSwim= true
+
+  }
+
+  makeSound() {
+    return `${this.name} says: ${this.sound}`;
+  }
+
+  move() {
+    const moves = [
+      `${this.name} flops across the beach like a giant wet noodle. `,
+      `${this.name} zooms underwater like a chunky torpedo with whiskers. `,
+      `${this.name} wiggles dramatically, demanding attention from literally everyone around.`
+    ];
+    return moves[Math.floor(Math.random() * moves.length)];
+  }
+
+  specialSkill() {
+    return `${this.name} balances a ball on its nose while striking a pose worthy of a circus superstar. `;
+  }
 }
+
+module.exports = SeaLion;
